3e2083ba00cd4c4c1591524ca8dded951acdfc51
Pablo Cuneo <pablocuneo@gmail.com>
2017-04-22 18:26:42 +0100

    -Added Sprites for most objects -Added Menu Template -Added UI Canvas

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
 	public Transform myTarget;
+	public float minY = 2f;
 
 	void Update () {
 		if(myTarget != null) {
 			Vector3 targPos = myTarget.position;
 			targPos.z = transform.position.z;
 
-			transform.position = Vector3.Lerp(transform.position,targPos, 0.1f);
+			Vector3	newPosition = Vector3.Lerp (transform.position, targPos, 0.1f);
+			newPosition.y = (newPosition.y < minY) ? minY : newPosition.y;
+			transform.position = newPosition;
 			//TODO: Prevent the camera from going too low (or too high)
 		}
 	}
---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour {
+
+    [SerializeField]
+    Image progressBarFull;
+
+    public float barProgress = 0f;
+
+    void Update () {
+		//Fill amount range 0-1
+		progressBarFull.fillAmount = barProgress;
+    }
+}
