ffcf7a951d2865fb54f91f0f77b3b6355bd703da
Pablo Cuneo <pablocuneo@gmail.com>
2017-04-22 14:44:40 +0100

    First draft of bee world

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform myTarget;
+
+	void Update () {
+		if(myTarget != null) {
+			Vector3 targPos = myTarget.position;
+			targPos.z = transform.position.z;
+
+			transform.position = Vector3.Lerp(transform.position,targPos, 0.1f);
+			//TODO: Prevent the camera from going too low (or too high)
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	void Start()
+	{
+	}
+		
+	void FixedUpdate()
+	{
+		Rigidbody2D rigidbody = GetComponent<Rigidbody2D> ();
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//transform.Rotate (new Vector3 (0f, 0f, -moveHorizontal), Space.Self);
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		rigidbody.AddForce (movement * speed);
+	}
+}
